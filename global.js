4b1ff26e175c075145be9215f7fa80b1b848df15
SyedOmarAli <syedomarali.smit@gmail.com>
2024-03-18 15:28:13 +0500

    add js

--- global.js
+++ global.js
@@ -1,5 +1,15 @@
-document.getElementsByClassName('btn-hamb').addEventListener("click", navDefaultMobile)
 function showNavBar() {
     const navDefaultMobile = document.querySelector('.nav-default-mobile');
-    navDefaultMobile.computedStyleMap.visibility =  'visible';
+    navDefaultMobile.style.visibility =  'visible'
+    navDefaultMobile.style.display =  'block'
+    navDefaultMobile.style.opacity =  '2'
+    navDefaultMobile.style.textAlign = 'center';
+    navDefaultMobile.style.left =  '0'
+    navDefaultMobile.style.width =  '100%'
+    navDefaultMobile.style.bottom =  '0'
+    navDefaultMobile.style.top =  '25px'
+}
+function closeFunc() {
+    const navDefaultMobile = document.querySelector('.nav-default-mobile');
+    navDefaultMobile.style.visibility =  'hidden'
 }
