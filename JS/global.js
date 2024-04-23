16473a68f5aabe63c0d4f6206845e407df9ccf46
SyedOmarAli <syedomarali.smit@gmail.com>
2024-04-23 20:19:31 +0500

    updated responsiveness

--- JS/global.js
+++ JS/global.js
@@ -1,15 +1,45 @@
 function showNavBar() {
     const navDefaultMobile = document.querySelector('.nav-default-mobile');
-    navDefaultMobile.style.visibility =  'visible'
-    navDefaultMobile.style.display =  'block'
-    navDefaultMobile.style.opacity =  '2'
+    navDefaultMobile.style.visibility = 'visible'
+    navDefaultMobile.style.display = 'block'
+    navDefaultMobile.style.opacity = '2'
     navDefaultMobile.style.textAlign = 'center';
-    navDefaultMobile.style.left =  '0'
-    navDefaultMobile.style.width =  '100%'
-    navDefaultMobile.style.bottom =  '0'
-    navDefaultMobile.style.top =  '25px'
+    navDefaultMobile.style.left = '0'
+    navDefaultMobile.style.width = '100%'
+    navDefaultMobile.style.bottom = '0'
+    navDefaultMobile.style.top = '25px'
 }
+
 function closeFunc() {
     const navDefaultMobile = document.querySelector('.nav-default-mobile');
-    navDefaultMobile.style.visibility =  'hidden'
+    navDefaultMobile.style.visibility = 'hidden'
 }
+
+
+const navDefault = document.querySelector('.nav-default'); 
+const articleHed = document.querySelector('.header-scrolling-article-hed');
+const headerNav = document.querySelector('.header__nav');
+const toggleSocialLinks = document.querySelector('.tg-social-links');
+const searchBox = document.querySelector('.serchBox');
+const headerDef = document.querySelector('.header__inner');
+articleHed.remove();
+toggleSocialLinks.remove();
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 10) {
+        navDefault.remove(); 
+        searchBox.remove();
+        headerDef.appendChild(toggleSocialLinks);
+        headerNav.appendChild(articleHed);
+    } else {
+        headerNav.appendChild(navDefault);        
+        headerDef.appendChild(searchBox);
+        toggleSocialLinks.remove();
+        articleHed.remove();
+    }
+
+}
+)
+
+
+
